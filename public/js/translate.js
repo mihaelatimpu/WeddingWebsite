a9029c473578ea6698326710ab646335492eb7b6
Mihaela <mihaela.cuna@gmail.com>
2019-10-08 18:45:48 +0200

    Fixed timer

--- public/js/translate.js
+++ public/js/translate.js
@@ -3,39 +3,26 @@ $(function(){
 	$(".dropdown-menu a").click(function(){
 		var cookieValue = $(this).text();
 		translatePage($(this).text());
-		//document.cookie = "language="+$(this).text();
-document.cookie = "lboxcook=" + encodeURIComponent(cookieValue);
+		document.cookie = "lboxcook=" + encodeURIComponent(cookieValue);
 
-   // alert('You have set the cookie: ' + $.cookie('language'));
-
- });
+	});
 });
 
 
 function getCookieData(cookie_name) {
-    var results = document.cookie.match ('(^|;) ?' + cookie_name + '=([^;]*)(;|$)');
-    return results ? decodeURIComponent(results[2]) : null;
+	var results = document.cookie.match ('(^|;) ?' + cookie_name + '=([^;]*)(;|$)');
+	return results ? decodeURIComponent(results[2]) : null;
 }
-/*
-
-function getCookieData( name ) {
-    var pairs = document.cookie.split("; "),
-        count = pairs.length, parts; 
-    while ( count-- ) {
-        parts = pairs[count].split("=");
-        if ( parts[0] === name )
-            return parts[1];
-    }
-    return false;
-}*/
 
 function initPage() {
 	var language = getCookieData("lboxcook");
-	translatePage(language);
+	if(language){
+		translatePage(language);
+	}
 }
 function translatePage(language) {
-		$("#languageDropdown").text(language);
-		$("#languageDropdown").val(language);
+	$("#languageDropdown").text(language);
+	$("#languageDropdown").val(language);
 
 }
 initPage();
