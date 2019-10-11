37dffbff8a85d1313f37e4ff3b670943824d2788
mihaelatimpu <mihaela.cuna@gmail.com>
2019-10-11 22:02:51 +0200

    Fixing db

--- public/js/queries.js
+++ public/js/queries.js
@@ -6,7 +6,7 @@ const pool = new Pool({
 	password: 'c3b4e7e6023b6b0dd3e0a1b6219233651510ebcad2edb381406cf429de4f4ecb',
 	port: 5432,
 })
-const presenceTable = 'presence';
+const presenceTable = 'confirmations';
 
 
 const getUsers = (request, response) => {
@@ -39,37 +39,7 @@ const addConfirmation = (request, response) => {
 
 	}
 
-
-	const createUser = (request, response) => {
-		const { name, email } = request.body
-
-		pool.query('INSERT INTO users (name, email) VALUES ($1, $2)', [name, email], (error, results) => {
-			if (error) {
-				throw error
-			}
-			response.status(201).send(`User added with ID: ${results.insertId}`)
-		})
-	}
-
-	const updateUser = (request, response) => {
-		const id = parseInt(request.params.id)
-		const { name, email } = request.body
-
-		pool.query(
-			'UPDATE users SET name = $1, email = $2 WHERE id = $3',
-			[name, email, id],
-			(error, results) => {
-				if (error) {
-					throw error
-				}
-				response.status(200).send(`User modified with ID: ${id}`)
-			}
-			)
-	}
-
 	module.exports = {
 		getUsers,
-		createUser,
 		addConfirmation,
-		updateUser,
 	}
