b80b6cfea83f7d3f9e166a2876a686f855d9c244
mihaelatimpu <mihaela.cuna@gmail.com>
2019-10-12 20:42:47 +0200

    Added french translations

--- public/js/language_fr.js
+++ public/js/language_fr.js
@@ -1,45 +1,68 @@
 var texts_fr = [
 
 /*header*/
-["text-we-are-getting-married", "We are getting married" ],
-["text-wedding-date-formatted", "2nd of May 2020" ],
-["text-remaining", "remaining" ],
-["text-days", "days" ],
+["text-we-are-getting-married", "Nous nous marions" ],
+["text-wedding-date-formatted", "Le 2 mai 2020" ],
+["text-remaining", "jours" ],
+["text-days", "restants" ],
 
 /*menu*/
-["text-home", "Home" ],
-["text-confirm", "Confirm your presence" ],
-["text-information", "Useful information" ],
-["text-wedding-photos", "Wedding photos" ],
-["text-contact-us", "Contact us" ],
-["text-language", "Language" ],
-["text-english", "English" ],
-["text-romanian", "Romanian" ],
-["text-french", "French" ],
+["text-home", "Acceuil" ],
+["text-confirm", "Répondre à l'invitation" ],
+["text-information", "Informations utiles" ],
+["text-wedding-photos", "Photos de mariage" ],
+["text-contact-us", "Nous contacter" ],
+["text-en", "Anglais" ],
+["text-ro", "Roumain" ],
+["text-fr", "Français" ],
 
 /*form-success*/
-["text-thank-you", "Thank you!" ],
-["text-data-submitted-success", "Your data has been successfully submitted. We look forward to seeing you at the wedding!" ],
+["text-thank-you", "Merci" ],
+["text-data-submitted-success", "Votre réponse a bien été soumise. Nous nous réjouissons de vous voir au mariage!" ],
 
-/*home*/
-["", "" ],
-["", "" ],
-["", "" ],
+/*confirm*/
+["text-please-choose-option", "Choisissez une option: " ],
+["text-coming", "Je viens" ],
+["text-not-coming", "Je ne viens pas" ],
+
+/*confirm-absence*/
+["text-name", "Votre nom" ],
+["text-reason", "La raison de votre absence? (optionnel)" ],
+["text-submit", "Soumettre" ],
 
 /*confirm-presence*/
-["", "" ],
-["", "" ],
-["", "" ],
-["", "" ],
+["text-confirm-presence", "Confirmer votre presence" ],
+["text-partner-name", "Le nome de votre partenaire (blanc si célibataire)" ],
+["text-children", "Enfants(age)" ],
+["text-email", "Email" ],
+["text-phone-number", "Numéro de téléphone" ],
+["text-coming-for", "Viens de la part de" ],
+["text-choose", "Choisissez..." ],
+["text-both", "les deux mariés" ],
+["text-none", "Aucun" ],
+["text-address", "Votre adresse (si vousdésirez recevoir une invitation papier)" ],
+["text-food-allergies", "Allergies" ],
+["text-comments", "Autres informations utiles?" ],
+
+/*error*/
+["text-could-not-find-page", "Page introuvable" ],
+["text-could-not-find-page-description", "Vous venez de détruire internet... Veuillez nous contacter pour que nous fixions le problème." ],
+
+/*form-absence-success*/
+["text-data-submitted-success-absence", "Votre réponse a bien été soumise. Nous sommes désolé que vous ne puissiez pas vous joindre à nous." ],
 
 /*information*/
-["", "" ],
-["", "" ],
-["", "" ],
+["text-we-will-update-later", "L'horaire de la cérémonie et autres informations seront mis à jour ici aussi vite que possible." ],
+["text-book-a-hotel", "Réserver votre hotel" ],
+["text-tel-bord-de-la-dhui", "tel: +33 662 170 115" ],
+["text-mail-jung", "mailTo: info@jungbrunnen-orges.com" ],
+["text-phone-jung", "tel: +33 681 646 394" ],
 ["", "" ],
 
-/*contact*/
-["", "" ],
-["", "" ],
-["", "" ]
+/*home*/
+["text-line-1", "Mihaela & Sébastien" ],
+["text-line-2", "se réjouissent de votre compagnie" ],
+["text-line-3", "lors de la célébration de leur mariage" ],
+["text-line-4", "Samedi" ],
+["text-line-5", "2 May 2020, 10h00" ]
 ];
